07a641617e9012f3fb9fc2388ff08e7b15358232
Arnaud Provoost <r0792836@student.thomasmore.be>
2022-06-22 18:30:42 +0200

    env file with token

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import json
+
 import discord
+import json
 from gpiozero import CPUTemperature
+
+f = open('env.json')
+data = json.load(f)
 
 bot = discord.Client()
 
 class cpu_class:
-	temperature = "I am running locally"
+	temperature = "0"
 
 @bot.event
 async def on_ready():
@@ -12,22 +18,22 @@ async def on_ready():
     for guild in bot.guilds:
         print(f"- {guild.id} (name: {guild.name})")
         guild_count = guild_count + 1
-    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+    print("DiscordBot is in " + str(guild_count) + " guilds.")
 
 
 @bot.event
 async def on_message(message):
     message_content = message.content
     if "temperature" in message_content:
-        await message.channel.send(getTemperatureRaspberrypi())
+        await message.channel.send("My cpu is at "+getTemperatureRaspberrypi()+"°c")
 
 
 def getTemperatureRaspberrypi():
     cpu = "null"
     # cpu = CPUTemperature()
     if cpu != "null":
-        cpu_class.temperature = cpu.temerature
+        cpu_class.temperature = round(cpu.temerature)
     return cpu_class.temperature
 
 
-bot.run("Change this")
+bot.run(data["token"])
