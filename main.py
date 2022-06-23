e1e9227c009bfa55314b2d7b9dc02833c4664124
Arnaud Provoost <r0792836@student.thomasmore.be>
2022-06-23 10:50:20 +0200

    stop command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 import discord
 import json
@@ -18,21 +19,24 @@ async def on_ready():
     for guild in bot.guilds:
         print(f"- {guild.id} (name: {guild.name})")
         guild_count = guild_count + 1
-    print("DiscordBot is in " + str(guild_count) + " guilds.")
+    print("DiscordBot is in " + str(guild_count) + " server.")
 
 
 @bot.event
 async def on_message(message):
     message_content = message.content
     if "temperature" in message_content:
-        await message.channel.send("My cpu is at "+getTemperatureRaspberrypi()+"°c")
+        await message.channel.send("My cpu is at "+ getTemperatureRaspberrypi()+"°c")
+    if "stop" in message_content:
+        await message.channel.send("I will stop myself")
+        sys.exit()
 
 
 def getTemperatureRaspberrypi():
     cpu = "null"
     # cpu = CPUTemperature()
     if cpu != "null":
-        cpu_class.temperature = round(cpu.temerature)
+        cpu_class.temperature = str(round(cpu.temperature))
     return cpu_class.temperature
 
 
